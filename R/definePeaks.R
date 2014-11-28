7763e311b06e81ca9e511e9d974698f0a92997ac
sheffien <sheffien@users.noreply.github.com>
2014-11-28 11:09:56 +0100

    Correctly allow for other window step values

--- R/definePeaks.R
+++ R/definePeaks.R
@@ -6,7 +6,7 @@ defineAllPeaks = function(chrom, scratchDir, windowSize, windowStep, windowCount
 		cat("\nChrom:", chrom, "\t");
 		wiggleFileName = paste(scratchDir, chrom, ".density.", windowSize, "-", windowStep, ".wig", sep="")
 		densityEstimate = scan(wiggleFileName, skip=1, allowEscapes=TRUE)
-		windowCoords = seq(from=51, by=5, length.out=windowCount[[chrom]])
+		windowCoords = seq(from=(windowSize+1), by=windowStep, length.out=windowCount[[chrom]])
 		peakCoords = defineBroadPeaks(chrom, densityEstimate, windowCoords, scratchDir, looseQuantile, windowStep);
 		if(!is.null(peakCoords))
 		defineNarrowPeaks(chrom, densityEstimate, windowCoords, scratchDir, peakCoords);
